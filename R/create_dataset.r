3f1dcf69b51d25f557f35595fcaa17e05677ab8f
Joel Barnes <j@barnzilla.ca>
2020-10-16 08:58:32 -0400

    Add new variables to the dataset and make the covariance matrix positive definite if it is not

--- R/create_dataset.r
+++ R/create_dataset.r
@@ -23,7 +23,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("w", "p", 27),
       get_correlation_coefficient("w", "n", 33),
       get_correlation_coefficient("w", "p", 40),
-      get_correlation_coefficient("s", "p", 48)
+      get_correlation_coefficient("s", "p", 48),
+      get_correlation_coefficient("w", "n", 57),
+      get_correlation_coefficient("w", "p", 67)
     ),
     gender = c(
       get_correlation_coefficient("w", "p", 1),
@@ -35,7 +37,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("s", "p", 28),
       get_correlation_coefficient("w", "p", 34),
       get_correlation_coefficient("s", "p", 41),
-      get_correlation_coefficient("s", "p", 49)
+      get_correlation_coefficient("s", "p", 49),
+      get_correlation_coefficient("m", "p", 58),
+      get_correlation_coefficient("m", "p", 68)
     ),
     ethnicity = c(
       get_correlation_coefficient("w", "p", 2),
@@ -47,7 +51,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("w", "p", 29),
       get_correlation_coefficient("w", "p", 35),
       get_correlation_coefficient("s", "p", 42),
-      get_correlation_coefficient("w", "p", 50)
+      get_correlation_coefficient("w", "p", 50),
+      get_correlation_coefficient("w", "p", 59),
+      get_correlation_coefficient("w", "p", 69)
     ),
     vo2max = c(
       get_correlation_coefficient("m", "n", 4),
@@ -59,7 +65,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("s", "p", 30),
       get_correlation_coefficient("w", "p", 36),
       get_correlation_coefficient("l", "p", 43),
-      get_correlation_coefficient("s", "n", 51)
+      get_correlation_coefficient("s", "n", 51),
+      get_correlation_coefficient("w", "p", 60),
+      get_correlation_coefficient("l", "p", 70)
     ),
     mvpa = c(
       get_correlation_coefficient("m", "n", 7),
@@ -71,7 +79,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("s", "p", 31),
       get_correlation_coefficient("s", "p", 37),
       get_correlation_coefficient("s", "p", 44),
-      get_correlation_coefficient("s", "n", 52)
+      get_correlation_coefficient("s", "n", 52),
+      get_correlation_coefficient("m", "p", 61),
+      get_correlation_coefficient("l", "p", 71)
     ),
     sprint = c(
       get_correlation_coefficient("s", "p", 22),
@@ -83,7 +93,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("s", "p", 32),
       get_correlation_coefficient("w", "p", 38),
       get_correlation_coefficient("m", "n", 45),
-      get_correlation_coefficient("m", "p", 53)
+      get_correlation_coefficient("m", "p", 53),
+      get_correlation_coefficient("s", "p", 62),
+      get_correlation_coefficient("m", "p", 72)
     ),
     confidence = c(
       get_correlation_coefficient("w", "p", 27),
@@ -95,7 +107,9 @@ get_dataset <- function(n = 1000) {
       1,
       get_correlation_coefficient("m", "p", 39),
       get_correlation_coefficient("s", "p", 46),
-      get_correlation_coefficient("m", "n", 54)
+      get_correlation_coefficient("m", "n", 54),
+      get_correlation_coefficient("w", "n", 63),
+      get_correlation_coefficient("w", "p", 73)
     ),
     wellbeing = c(
       get_correlation_coefficient("w", "n", 33),
@@ -107,9 +121,11 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("m", "p", 39),
       1,
       get_correlation_coefficient("w", "p", 47),
-      get_correlation_coefficient("s", "n", 55)
+      get_correlation_coefficient("s", "n", 55),
+      get_correlation_coefficient("w", "p", 64),
+      get_correlation_coefficient("s", "p", 74)
     ),
-    status = c(
+    caffeine = c(
       get_correlation_coefficient("w", "p", 40),
       get_correlation_coefficient("s", "p", 41),
       get_correlation_coefficient("s", "p", 42),
@@ -119,7 +135,9 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("s", "p", 46),
       get_correlation_coefficient("w", "p", 47),
       1,
-      get_correlation_coefficient("s", "n", 56)
+      get_correlation_coefficient("s", "n", 56),
+      get_correlation_coefficient("w", "p", 65),
+      get_correlation_coefficient("m", "p", 75)
     ),
     bmi = c(
       get_correlation_coefficient("s", "p", 48),
@@ -131,19 +149,57 @@ get_dataset <- function(n = 1000) {
       get_correlation_coefficient("m", "n", 54),
       get_correlation_coefficient("s", "n", 55),
       get_correlation_coefficient("s", "n", 56),
+      1,
+      get_correlation_coefficient("w", "p", 66),
+      get_correlation_coefficient("m", "n", 76)
+    ),
+    push_ups = c(
+      get_correlation_coefficient("w", "n", 57),
+      get_correlation_coefficient("m", "p", 58),
+      get_correlation_coefficient("w", "p", 59),
+      get_correlation_coefficient("w", "p", 60),
+      get_correlation_coefficient("m", "p", 61),
+      get_correlation_coefficient("s", "p", 62),
+      get_correlation_coefficient("w", "n", 63),
+      get_correlation_coefficient("w", "p", 64),
+      get_correlation_coefficient("w", "p", 65),
+      get_correlation_coefficient("w", "p", 66),
+      1,
+      get_correlation_coefficient("s", "p", 77)
+    ),
+    heart_rate_time1 = c(
+      get_correlation_coefficient("w", "p", 67),
+      get_correlation_coefficient("m", "p", 68),
+      get_correlation_coefficient("w", "p", 69),
+      get_correlation_coefficient("l", "p", 70),
+      get_correlation_coefficient("l", "p", 71),
+      get_correlation_coefficient("m", "p", 72),
+      get_correlation_coefficient("w", "p", 73),
+      get_correlation_coefficient("s", "p", 74),
+      get_correlation_coefficient("m", "p", 75),
+      get_correlation_coefficient("m", "n", 76),
+      get_correlation_coefficient("s", "p", 77),
       1
     )
   )
 
   # Add row names that are identical to column names (nice to have when viewing the covariance matrix)
-  row.names(cov_mat) <- colnames(cov_mat)
+  row.names(cov_mat) <- cnames <- colnames(cov_mat)
+
+  # Convert cov_mat data frame to a matrix
+  cov_mat <- unname(as.matrix(cov_mat))
+
+  # Make cov_mat positive definite if it is not
+  if(isFALSE(is.positive.definite(cov_mat))) cov_mat <- make.positive.definite(cov_mat)
 
   # Generate correlated dataset
+  n = 1000
   d <- genCorData(
-	n, 
-	mu = c(25, 0.5, 1, 42, 4, 13, 4.5, 3.8, 0.1, 22), sigma = c(2, 0.1, 3, 3, 1, 2, 1.1, 1.1, 1, 1.5), 
-	corMatrix = unname(as.matrix(cov_mat)), 
-	cnames = "age,gender,ethnicity,vo2max,mvpa,sprint,confidence,wellbeing,caffeine,bmi"
+    n,
+    mu = c(25, 0.5, 1, 42, 4, 13, 4.5, 3.8, 0.1, 22, 25, 65),
+    sigma = c(2, 0.1, 3, 3, 1, 2, 1.1, 1.1, 1, 1.5, 5, 10),
+    corMatrix = cov_mat,
+    cnames = cnames
   )
 
   # Data wrangling
